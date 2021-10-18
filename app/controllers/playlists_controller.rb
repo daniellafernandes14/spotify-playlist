4b2da74f098c3c88d5cec25436e6a8ddcd064bce
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-10-18 16:28:02 +0100

    Added controllers to app

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,19 @@
+class PlaylistsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
