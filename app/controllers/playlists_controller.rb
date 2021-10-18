408efff6634b93145827c1d6fd630b84be5450f1
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-10-18 16:38:52 +0100

    Update playlist controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,19 +1,45 @@
 class PlaylistsController < ApplicationController
+
+  before_action :find_playlist, only: [:show, :edit, :update, :destroy]
+
   def index
+    Playlist.all = @playlists
   end
 
   def show
   end
 
+  def new
+    @playlist = Playlist.new
+  end
+
   def create
+    @playlist = Playlist.new(playlist_params)
+    @playlist.save
+    redirect_to playlist_path(@playlist)
+  end
+
+  def edit
   end
 
   def update
-  end
-
-  def new
+    @playlist.update(playlist_params)
+    redirect_to playlist_path(@playlist)
   end
 
   def destroy
+    @playlist.destroy
+    redirect_to playlist_path
   end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name)
+  end
+
+  def find_playlist
+    @playlist = Playlist.find(params[:id])
+  end
+
 end
