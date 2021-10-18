3f29d412e5a0b0802f79224053352bc4db1b89fe
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-10-18 16:57:36 +0100

    Added cors.rb file to allow spotify API to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#index'
   resources :playlists do
     resources :songs, only: [ :show ]
   end
-
-  get '*path', to: 'pages#index', via: :all # route requests that arent for existing paths that arent defined in our API back to our index path.
 end
