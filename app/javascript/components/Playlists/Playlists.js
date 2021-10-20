e6f2f4f7cb65c6df95bceadeea191b5df7cf5ddb
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-10-20 17:46:01 +0100

    Attempted API

--- app/javascript/components/Playlists/Playlists.js
+++ app/javascript/components/Playlists/Playlists.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Playlists = () => {
-  return <div>This is the Playlists index view for our app.</div>
+  const[playlists, setPlaylists] = useState([])
+
+  useEffect(()=>{
+    axios.get('<api url e.g. /api/v1/airlines.json>')
+    .then( resp => console.log(resp) )
+    .catch( resp => console.log(resp) )
+  }, [playlists.length])
+
+  return (
+  <div>This is the Playlists index view for our app.</div>
+  )
 }
 
 export default Playlists
